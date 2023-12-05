150c418b45754397897b99937f8a08ae25105a7a
azkifairuz <azkiajmal@gmail.com>
2023-12-05 17:10:35 +0700

    controller login

--- controller/authController.js
+++ controller/authController.js
@@ -1,16 +1,19 @@
 const { user } = require("../models");
 const { auth } = require("../models");
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { responseMessage, responseData } = require("../utils/responseHandle");
 function login(req, res) {}
 
 async function register(req, res) {
   try {
     const { name, username, email, password } = req.body;
-    console.log(name,username,email,password);
+    const passwordHash =  await bcrypt.hash(password, 10)
+    console.log(name,username,email, await bcrypt.hash(password, 10));
     const newUser = await user.create({ name: name, username: username });
     const newAccount = await auth.create({
       email: email,
-      password: password,
+      password: passwordHash,
       id_user: newUser.id,
     });
     responseData(
@@ -29,7 +32,38 @@ async function register(req, res) {
   }
 }
 
-function login(req, res) {}
+async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+
+    // Find the user by email
+    const user = await auth.findOne({ where: { email } });
+
+    if (!user) {
+      return responseMessage(res, 401, "Invalid credentials");
+    }
+
+    // Check if the password is correct
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return responseMessage(res, 401, "Invalid credentials");
+    }
+
+    // Generate a JWT
+    const token = jwt.sign({ userId: user.id }, 'a40a47053167cb92bcb9b46ceff99ae2b734f758fbd565b1d70fb73ca2c16458', { expiresIn: '1h' });
+
+    // Update the token in the database
+    await auth.update({ token }, { where: { id: user.id } });
+
+    // Respond with the token or any other relevant information
+    responseData(res, 200, { token }, "Login successful");
+  } catch (error) {
+    console.error(error);
+    responseMessage(res, 500, "Internal server error");
+  }
+}
+
 function updateAkun(req, res) {}
 
 function deleteAkun(req, res) {}
