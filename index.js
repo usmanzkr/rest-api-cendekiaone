55f17b693aad2de68c8719e2864250b4b299d4d3
azkifairuz <azkiajmal@gmail.com>
2023-12-05 16:45:34 +0700

    cors

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
+const cors = require('cors');
 const userRoute = require("./router/userRoute"); 
 const authRoute = require("./router/authRoute"); 
 const dotenv = require("dotenv");
 dotenv.config();
 const PORT = process.env.PORT || 5000;
+
+// Enable CORS for all routes
+app.use(cors());
+
 // set body parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-//set route
 
+//set route
 //user
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
